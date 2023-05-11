5365c45fbe973337c945220fe8480c3e02fa04b3
Luiz Miguel <luizmgc1930@gmail.com>
2023-05-11 17:07:55 -0300

    index222

--- src/index2.py
+++ src/index2.py
@@ -20,6 +20,8 @@ G = nx.Graph()
 posicoes_PI = []
 posicoes_SIGMA = []
 posicoes_TABLES = []
+posicoes_UNION = []
+quant_UNION = 0
 for i in range(len(string)):
     if string[i] == "PI(":
         posicoes_PI.append(i)
@@ -29,7 +31,14 @@ for i in range(len(string)):
         if string[i] == detectFolhas.format(query["TABLES"][x]):
             posicoes_TABLES.append(i)
 print("Posicoes PI:", posicoes_PI)
+print("UNION:", query['UNION'])
 
+
+for i, elemento in enumerate(query["UNION"]):
+    if elemento == "|X|":
+        quant_UNION += 1
+        posicoes_UNION.append(i)
+print(posicoes_UNION)
 
 for i in range(len(queryTable)):
     bat = string[posicoes_SIGMA[i] : posicoes_TABLES[i]]
@@ -43,15 +52,34 @@ for i in range(len(queryTable)):
                 save = bat[x]
 
     G.add_edge(save, posicoes_PI[i + 1])
-    G.add_edge(posicoes_PI[i + 1], query["ON_SELECTION"][0])
-    G.add_edge(posicoes_PI[i + 1], query["ON_SELECTION"][0])
-else:
-    G.add_edge(query["ON_SELECTION"][0], posicoes_PI[0])
 
+quant = 0
+for i in range(len(queryTable)):
+    if len(queryTable) > 2:
+        if (i + 1) == len(queryTable):
+            print('eae')
+            save = query['ON_SELECTION'][quant]
+            quant += 1
+            G.add_edge(posicoes_PI[i + 1], query['ON_SELECTION'][quant])
+            G.add_edge(save, query['ON_SELECTION'][quant])
+        else:
+            G.add_edge(posicoes_PI[i + 1], query['ON_SELECTION'][quant])
+
+    else:
+        G.add_edge(posicoes_PI[i + 1], query["ON_SELECTION"][quant])
+        G.add_edge(posicoes_PI[i + 1], query["ON_SELECTION"][quant])
+        print('eae')
+else:
+    G.add_edge(query["ON_SELECTION"][quant], posicoes_PI[0])
+
+
+dfs_tree = nx.dfs_tree(G, source=0)
+
+print(dfs_tree)
 nx.draw(G, with_labels=True)
 print("Lista de nos: ", list(G.nodes()))
 print("Lista de arestas: ", list(G.edges()))
-print(string)
+#print(string)
 plt.show()
 
 #print(' '.join(optimizer.optimize_query()))
